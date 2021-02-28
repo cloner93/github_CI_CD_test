51790c5f8955edf6fc88682c85db8792b0d770df
milad <targholi2112@gmail.com>
2021-02-28 22:56:00 +0330

    inti...

---
+++ app/src/main/java/com/milad/github_ci_cd_test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.milad.github_ci_cd_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
